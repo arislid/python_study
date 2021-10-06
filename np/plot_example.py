cc4a09275054d2366c66ea5a0ff19e1ee30da453
arislid <arislid@gmail.com>
2021-10-06 22:51:43 +0900

    commit 2021 10 06

---
+++ np/plot_example.py
@@ -0,0 +1,15 @@
+'''
+practice Matplotlib in wikidocs.net/92071
+'''
+from matplotlib import pyplot as plt
+
+plt.plot([1, 2, 3, 4])
+plt.show()
+
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
+plt.show()
+
+#%%
+plt.plot([1, 2, 3, 4], [sqrt(1), sqrt(2), sqrt(3), sqrt(4)])
+plt.show()
+# %%
